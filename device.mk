90708fa875b9c032d412621ff04d29fbf17bd355
Daniel Micay <danielmicay@gmail.com>
2023-10-07 14:29:26 +0200

    disable GSI keys
    Change-Id: If656992264ffdc4a07645938d20f6ac87d00c1f9

--- device.mk
+++ device.mk
@@ -100,9 +100,6 @@ TARGET_PRODUCT_PROP := $(LOCAL_PATH)/product.prop
 
 $(call inherit-product, $(LOCAL_PATH)/utils.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
-
 ifeq ($(TARGET_PREBUILT_KERNEL),)
     LOCAL_KERNEL := device/google/sunfish-kernel/Image.lz4
 else
