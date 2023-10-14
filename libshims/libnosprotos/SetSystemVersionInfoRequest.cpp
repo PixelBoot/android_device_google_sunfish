70f624085c39dddf16b0e3661f00d21d4f48d08b
PixelBoot <bootingpixels@gmail.com>
2023-10-14 14:35:19 +0200

    sunfish: don't build shims locally

--- libshims/libnosprotos/SetSystemVersionInfoRequest.cpp
+++
@@ -1,5 +0,0 @@
-#include "nugget/app/keymaster/keymaster.pb.h"
-
-extern "C" void _ZN6nugget3app9keymaster27SetSystemVersionInfoRequestC1Ev(nugget::app::keymaster::SetSystemVersionInfoRequest* request) {
-    new (request) nugget::app::keymaster::SetSystemVersionInfoRequest;
-}
